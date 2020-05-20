419c3e4169b4d257d82ae81dcefd25eed4c864f3
mm0202 <for_etc@hotmail.com>
2020-05-20 14:03:05 +0900

    SB-215 fix

---
+++ github.com/mm0202-sandbox/template_go-package/HelloWorld_test.go
@@ -0,0 +1,20 @@
+package sample
+
+import (
+  "testing"
+  "strings"
+)
+
+func TestMain(t *testing.T) {
+	if strings.EqualFold("Hoge!!", HelloWorld()) {
+		t.Errorf("error!") 
+	}
+
+	if strings.EqualFold("Fuga!!", HelloWorld()) {
+		t.Errorf("error!") 
+	}
+
+  	if !strings.EqualFold("Hello World!!", HelloWorld()) {
+    	t.Errorf("error!") 
+  	}
+}
